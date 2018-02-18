5b151566391ea30ec9ae45972f076dc1f384aa54
phi-line <phi.liney@gmail.com>
2018-02-18 07:54:08 -0800

    added profile picture and short description to data package

--- api/crunchbase.py
+++ api/crunchbase.py
@@ -24,37 +24,41 @@ def main():
     # investor = cb.organization('sequoia-capital')
 
     res = requests.get(f'https://api.crunchbase.com/v3.1/organizations/{ORG}/investments?user_key={USER_KEY}')
-    j = loads(res.content) # first load of the data
+    j = loads(res.content)# first load of the data
 
     total_pages = j['data']['paging']['number_of_pages']
     current = 1
 
     Data = namedtuple('Data', ['name',
                                'description',
+                               'short_description',
+                               'profile_image_url',
                                'email',
                                'num_employees_max',
                                'num_employees_min',
                                'money_raised_usd',
                                'series'])
-    
+
 
     table = db.table(f"{ORG}")
-    while current < total_pages:
+    while current <= total_pages:
         for i in j['data']['items']:
             d = Data(i['relationships']['funding_round']['relationships']['funded_organization']['properties']['name'],
                      i['relationships']['funding_round']['relationships']['funded_organization']['properties']['description'],
+                     i['relationships']['funding_round']['relationships']['funded_organization']['properties']['short_description'],
+                     i['relationships']['funding_round']['relationships']['funded_organization']['properties']['profile_image_url'],
                      i['relationships']['funding_round']['relationships']['funded_organization']['properties']['contact_email'],
                      i['relationships']['funding_round']['relationships']['funded_organization']['properties']['num_employees_max'],
                      i['relationships']['funding_round']['relationships']['funded_organization']['properties']['num_employees_min'],
                      i['relationships']['funding_round']['properties']['money_raised_usd'],
                      i['relationships']['funding_round']['properties']['series'])
             table.insert(d._asdict())
-            print(d)
+            # print(d)
         res = requests.get(j['data']['paging']['next_page_url'], params={'user_key': USER_KEY})
         j = loads(res.content)
         current += 1
 
-    # pp = pprint.PrettyPrinter(j, depth=6)
+    print(f'Loaded {len(table)} companies.')
 
 
 if __name__ == "__main__":
