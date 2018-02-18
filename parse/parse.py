de58f6a7c61d462db132f8efa483303a47652639
Mojiashen1 <mojiashen1@gmail.com>
2018-02-17 20:17:58 -0800

    solve git issue

--- parse/parse.py
+++ parse/parse.py
@@ -1,14 +1,10 @@
 from pdftxt import convert_pdf_to_txt
 from indexer import return_index
 
-def main():
-  data_path = 'parse/resumes/'
-  input_path = data_path + 'fResume.pdf'
+def parse(resume_name):
+  input_path = 'parse/resumes/'+resume_name
   output_path = 'parse/outputtext.txt'
   convert_pdf_to_txt(input_path, output_path)
   indexed = return_index(output_path)
   # print(indexed)
   return indexed
-
-if __name__ == "__main__":
-  main()
